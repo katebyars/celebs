65224a3970cbc0ba0eaa97b542195171109588d4
Kate Byars and collin person <kate+collin@>
2017-07-06 16:39:55 -0700

    add a dropdown menu to star the celebrity crush selection process.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function()  {
+  var 1 = parseInt("1")
+  var 2 = parseInt("2")
+  var 3 = parseInt("3")
+  var 4 = parseInt("4")
+  var 5 = parseInt("5")
+  var 6 = parseInt("6")
+  var 7 = parseInt("7")
+  var 8 = parseInt("8")
+  var 9 = parseInt("9")
+  var 10 = parseInt("10")
+  $("#number").change(function(event)  {
+    if (number <= 3){
+      $("#oddities").show();
+  //  } else if (number > 3 < 7) {
+  //     ("#shapes").show();
+  }
+  });
+});
