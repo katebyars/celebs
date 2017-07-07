3bed1a4ee00de4d0e18bca916898c505580d5d16
collin person and Kate Byars <collin@>
2017-07-06 17:25:16 -0700

    add hid and show feature to dropdown.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
 $(document).ready(function()  {
-  var 1 = parseInt("1")
-  var 2 = parseInt("2")
-  var 3 = parseInt("3")
-  var 4 = parseInt("4")
-  var 5 = parseInt("5")
-  var 6 = parseInt("6")
-  var 7 = parseInt("7")
-  var 8 = parseInt("8")
-  var 9 = parseInt("9")
-  var 10 = parseInt("10")
-  $("#number").change(function(event)  {
-    if (number <= 3){
-      $("#oddities").show();
+
+  $("#formOne").change(function(event) {
+
+    var number = $("#number").val();
+    parseInt(number);
+
+    if (number<=3){
+      $('#formTwo').show();
+      $('#formThree').hide();
+      $('#formFour').hide();
+    } else if (number == 4) {
+      $('#formThree').show();
+      $('#formTwo').hide();
+      $('#formFour').hide();
+    } else {
+      $('#formFour').show();
+      $('#formThree').hide();
+      $('#formTwo').hide();
+    }
   //  } else if (number > 3 < 7) {
   //     ("#shapes").show();
-  }
   });
 });
